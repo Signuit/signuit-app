9456458927629c3a818984b879c292e436ac555f
Ali Tap <atap2629@gmail.com>
2026-04-22 17:32:59 +0300

    chore: remove example application directory

--- apps/example/src/app/explorer/page.tsx
+++
@@ -1,7 +0,0 @@
-import { PackageExplorer } from "../../components/PackageExplorer";
-
-export const dynamic = "force-dynamic";
-
-export default async function ExplorerPage() {
-	return <PackageExplorer />;
-}
--- apps/example/src/app/history/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LiveActivityFeed } from "../../components/LiveActivityFeed";
-import { resolveServerSession } from "../../lib/nexus";
-
-export const dynamic = "force-dynamic";
-
-export default async function HistoryPage() {
-	const { partyId } = await resolveServerSession();
-	return <LiveActivityFeed partyId={partyId} />;
-}
